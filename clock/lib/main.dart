ff8fe863b619f969aac75c6aac6264839431a1be
Lars Vogel <Lars.Vogel@vogella.com>
2020-02-19 11:34:19 +0100

    Update clock

--- clock/lib/main.dart
+++ clock/lib/main.dart
@@ -49,7 +49,7 @@ class ClockFrame extends StatelessWidget {
                     ],
                   ),
                 ),
-                ClockFrame(),
+                ClockFace(),
               ]))
         ],
       ),
